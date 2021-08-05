6bb1fa85f2a2544cf475da8d2854ee31cbad8fc6
Elias Haileselassie <eliaszx@gmail.com>
2021-08-04 23:44:49 -0400

    method name postfix Async

--- src/main/kotlin/example/example.kt
+++ src/main/kotlin/example/example.kt
@@ -1,47 +1,67 @@
-package webirr.example
-
 /**
- * example.kt
  * Creating a new Bill / Updating an existing Bill on WeBirr Servers
  */
+package webirr.example
 
 import webirr.*;
 
 fun main() {
 
-    val apiKey = "YOUR_API_KEY"
-    val merchantId = "YOUR_MERCHANT_ID"
+    //val apiKey = "YOUR_API_KEY"
+    //val merchantId = "YOUR_MERCHANT_ID"
+
+    val apiKey =  System.getenv("wb_apikey_1") ?: ""
+    val merchantId =  System.getenv("wb_merchid_1") ?: ""
 
     val api = WeBirrClient(apiKey, true)
 
     var bill = Bill(
-        "270.90",
         "cc01",
         "Elias Haileselassie",
+        "kt/2021/130",
         "2021-07-22 22:14",
         "hotel booking",
-        "kt/2021/130",
-        merchantId
+        "270.90",
+        merchantId,
     )
 
     println("Creating Bill...")
 
-    api.createBill(bill) { it ->
+    api.createBillAsync(bill) { it ->
 
         if (it.error == null) {
             // success
-            val paymentCode = it.res ?: ""; // returns paymentcode such as 429 723 975
-            println("Payment Code = ${paymentCode}"); // we may want to save payment code in local db.
+            val paymentCode = it.res ?: "" // returns paymentcode such as 429 723 975
+            println("Payment Code = ${paymentCode}") // we may want to save payment code in local db.
 
         } else {
             // fail
-            println("error: ${it.error}");
-            println("errorCode: ${it.errorCode}"); // can be used to handle specific busines error such as ERROR_INVLAID_INPUT_DUP_REF
+            println("error: ${it.error}")
+            println("errorCode: ${it.errorCode}") // can be used to handle specific busines error such as ERROR_INVLAID_INPUT_DUP_REF
         }
     }
 
-// the above method call is async!
+    // the above method call is async!
     Thread.sleep(2000)
+
+    // update existing bill if it is not paid
+    bill.amount = "278.00"
+    bill.customerName = "Elias kotlin"
+    //bill.billReference = "WE SHOULD NOT CHANGE THIS";
+
+    println("Updating Bill...");
+    api.updateBillAsync(bill) { it ->
+
+        if (it.error == null) {
+            // success
+            println("bill is updated successfully")  //it.res will be 'OK'  no need to check here!
+
+        } else {
+            // fail
+            println("error: ${it.error}")
+            println("errorCode: ${it.errorCode}") // can be used to handle specific busines error such as ERROR_INVLAID_INPUT
+        }
+    }
 
 }
 
